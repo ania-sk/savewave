2309ad821e2e88897eabf8d88ca8d5597134ca07
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-05-15 14:57:29 +0200

    Escaping data

--- src/App/views/partials/_header.php
+++ src/App/views/partials/_header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>SaveWave &mdash; <?php echo $title ?></title>
+    <title>SaveWave &mdash; <?php echo e($title) ?></title>
     <link rel="icon" href="/assets/imgs/save-wave-circle.png" />
     <link rel="stylesheet" href="/assets/global.css" />
     <link rel="stylesheet" href="/assets/main.css" />
