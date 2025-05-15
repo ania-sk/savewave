42ef84e4ab72d80744d440d0eb230a328352969e
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-05-15 11:27:27 +0200

    Rendering a template

--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -7,4 +7,9 @@ namespace Framework;
 class TemplateEngine
 {
     public function __construct(private string $basePath) {}
+
+    public function render(string $template)
+    {
+        include "{$this->basePath}/{$template}";
+    }
 }
