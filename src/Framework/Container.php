e9e23fc4daac5d0668e2eda994b1bfb25f211b43
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-05-16 13:03:20 +0200

    Validating parameters

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Framework;
 
-use ReflectionClass;
+use ReflectionClass, ReflectionNamedType;
 use Framework\Exceptions\ContainerException;
 
 class Container
@@ -34,6 +34,21 @@ class Container
 
         if (count($params) === 0) {
             return new $className;
+        }
+
+        $dependencies = [];
+
+        foreach ($params as $param) {
+            $name = $param->getName();
+            $type = $param->getType();
+
+            if (!$type) {
+                throw new ContainerException("Failed to resolve class {$className} because {$name} is missing a type hint.");
+            }
+
+            if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
+                throw new ContainerException("Failed to resolve class {$className} because invalid param name.");
+            }
         }
 
         dd($params);
