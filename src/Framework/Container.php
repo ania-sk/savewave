680955f78773b6d3f5e4f28090aff4a6bddd33de
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-05-16 12:45:38 +0200

    Validating the constructor method

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -24,6 +24,12 @@ class Container
             throw new ContainerException("Class {$className} in not instantiable");
         }
 
-        dd($reflectionClass);
+        $constructor = $reflectionClass->getConstructor();
+
+        if (!$constructor) {
+            return new $className;
+        }
+
+        dd($constructor);
     }
 }
