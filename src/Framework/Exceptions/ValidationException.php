9d8c5c535a728ef84a4d7b8837258cfb9f407545
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-05-18 12:11:50 +0200

    Custom middleware

--- src/Framework/Exceptions/ValidationException.php
+++ src/Framework/Exceptions/ValidationException.php
@@ -6,4 +6,10 @@ namespace Framework\Exceptions;
 
 use RuntimeException;
 
-class ValidationException extends RuntimeException {}
+class ValidationException extends RuntimeException
+{
+    public function __construct(int $code = 422)
+    {
+        parent::__construct(code: $code);
+    }
+}
