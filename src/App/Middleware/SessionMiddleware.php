c56dd1f6d2a972d0c1ad562085c4d10381aafa6a
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-05-18 13:57:02 +0200

    Handling session exception

--- src/App/Middleware/SessionMiddleware.php
+++ src/App/Middleware/SessionMiddleware.php
@@ -5,11 +5,20 @@ declare(strict_types=1);
 namespace App\Middleware;
 
 use Framework\Contracts\MiddlewareInterface;
+use App\Exceptions\SessionException;
 
 class SessionMiddleware implements MiddlewareInterface
 {
     public function process(callable $next)
     {
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            throw new SessionException("Session alredy active.");
+        }
+
+        if (headers_sent()) {
+            throw new SessionException("Headers already sent.");
+        }
+
         session_start();
 
         $next();
