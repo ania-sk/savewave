af1d7f3f4b33376dcd895c720d2e924e9f18a97d
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-05-20 16:33:23 +0200

    Regenerating a session ID

--- src/App/Services/UserService.php
+++ src/App/Services/UserService.php
@@ -58,6 +58,8 @@ class UserService
             throw new ValidationException(['password' => ['Invalid credentials']]);
         }
 
+        session_regenerate_id();
+
         $_SESSION['user'] = $user['id'];
     }
 }
