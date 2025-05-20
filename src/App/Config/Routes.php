567264c26fb251f4f308dc0bd9056da7a250922b
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-05-20 16:52:58 +0200

    Applying route middleware

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -7,15 +7,16 @@ namespace App\Config;
 use Framework\App;
 
 use App\Controllers\{HomeController, MainPageController, IncomesController, ExpensesController, AuthController};
+use App\Middleware\{AuthRequiredMiddleware, GuestOnlyMiddleware};
 
 function registerRoutes(App $app)
 {
     $app->get('/', [HomeController::class, 'home']);
-    $app->get('/mainPage', [MainPageController::class, 'mainPage']);
+    $app->get('/mainPage', [MainPageController::class, 'mainPage'])->add(AuthRequiredMiddleware::class);
     $app->get('/incomes', [IncomesController::class, 'incomes']);
     $app->get('/expenses', [ExpensesController::class, 'expenses']);
-    $app->get('/register', [AuthController::class, 'registerView']);
-    $app->post('/register', [AuthController::class, 'register']);
-    $app->get('/login', [AuthController::class, 'loginView']);
-    $app->post('/login', [AuthController::class, 'login']);
+    $app->get('/register', [AuthController::class, 'registerView'])->add(GuestOnlyMiddleware::class);
+    $app->post('/register', [AuthController::class, 'register'])->add(GuestOnlyMiddleware::class);
+    $app->get('/login', [AuthController::class, 'loginView'])->add(GuestOnlyMiddleware::class);
+    $app->post('/login', [AuthController::class, 'login'])->add(GuestOnlyMiddleware::class);
 }
