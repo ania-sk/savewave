a19604c8a963f9b63b8779f16afaf48a0020d1f7
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-05-21 11:14:04 +0200

    Creating tables in database

--- database.sql
+++ database.sql
@@ -8,3 +8,29 @@ CREATE TABLE IF NOT EXISTS users(
     PRIMARY KEY(id),
     UNIQUE KEY(email)
 ); 
+
+CREATE TABLE IF NOT EXISTS incomes_category_default(
+    id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS incomes_category_assigned_to_users(
+    id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id BIGINT(20) UNSIGNED NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS incomes(
+    id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id BIGINT(20) UNSIGNED NOT NULL,
+    income_category_assigned_to_user_id BIGINT(20) UNSIGNED NOT NULL,
+    amount DECIMAL(8,2),    
+    date_of_income DATETIME NOT NULL,
+    income_comment VARCHAR(255),
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(income_category_assigned_to_user_id) REFERENCES incomes_category_assigned_to_users(id)
+);
