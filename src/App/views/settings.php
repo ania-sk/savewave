b01328f2db61ebb5f3843b954218ed474867f540
ania-sk <127092221+ania-sk@users.noreply.github.com>
2025-06-24 19:03:14 +0200

    Adding account details section in settings

--- src/App/views/settings.php
+++ src/App/views/settings.php
@@ -16,10 +16,10 @@ include $this->resolve("partials/_sideNavAndModals.php");
         </div>
 
         <!-- INCOME/EXPENSE CATEGORIES -->
-        <div class="section-category-set  grid-cols-2 ">
+        <section class="section-category-set  grid-cols-2 ">
             <!-- INCOME CATEGORIES -->
             <div>
-                <div class="heading-secondary">
+                <div class="heading-tertiary">
                     <p>Income categories</p>
                 </div>
 
@@ -47,14 +47,22 @@ include $this->resolve("partials/_sideNavAndModals.php");
 
                     </table>
 
-                    <button id="add-income-category-btn" class="btn btn--modal">Add new categorie</button>
-
+                </div>
+                <!-- add new income form -->
+                <div class="add-category-form">
+                    <form action="/settings/addIncomeCategory" method="post">
+                        <div class="category-field">
+                            <label for="income-category">New Income Category:</label>
+                            <input type="text" id="income-category" name="categoryName" placeholder="Enter new income category" required>
+                        </div>
+                        <button id="add-income-category-btn" class="btn btn--modal">Add new categorie</button>
+                    </form>
                 </div>
             </div>
 
             <!-- EXPENSE CATEGORIES -->
             <div>
-                <div class="heading-secondary">
+                <div class="heading-tertiary">
                     <p>Expense categories</p>
                 </div>
 
@@ -88,10 +96,71 @@ include $this->resolve("partials/_sideNavAndModals.php");
                             </tr>
                         </tbody>
                     </table>
-                    <button id="add-expense-category-btn" class="btn btn--modal">Add new categorie</button>
+
+                </div>
+                <!-- add new expense form -->
+                <div class="add-category-form">
+                    <form action="/settings/addExpenseCategory" method="post">
+                        <div class="category-field">
+                            <label for="expense-category">New Expense Category:</label>
+                            <input type="text" id="expense-category" name="categoryName" placeholder="Enter new expense category" required>
+                        </div>
+                        <button id="add-expense-category-btn" class="btn btn--modal" type="submit">Add new categorie</button>
+                    </form>
                 </div>
             </div>
-        </div>
+        </section>
+
+        <!-- ACCOUNT DETAILS -->
+        <section class="account-sec flex-conteiner">
+            <div class="account-details-container">
+                <div class="heading-tertiary">
+                    <p>Account Details</p>
+                </div>
+
+                <!--  Email -->
+                <div class="account-edit-section">
+                    <form class="account-form" action="/settings/updateEmail" method="post">
+                        <div class="account-field">
+                            <label for="email">Email:</label>
+                            <input type="email" id="email" name="email" value="" required>
+                        </div>
+                        <button type="submit" class="btn btn--full btn--modal">Update Email</button>
+                    </form>
+                </div>
+
+                <!--  Username -->
+                <div class="account-edit-section">
+                    <form class="account-form" action="/settings/updateUsername" method="post">
+                        <div class="account-field">
+                            <label for="username">Username:</label>
+                            <input type="text" id="username" name="username" value="" required>
+                        </div>
+                        <button type="submit" class="btn btn--full btn--modal">Update Username</button>
+                    </form>
+                </div>
+
+                <!-- Password -->
+                <div class="account-edit-section">
+                    <form class="account-form" action="/settings/updatePassword" method="post">
+                        <div class="account-field">
+                            <label for="password">New Password:</label>
+                            <input type="password" id="password" name="password" placeholder="Enter new password">
+                        </div>
+                        <button type="submit" class="btn btn--full btn--modal">Update Password</button>
+                    </form>
+                </div>
+
+                <!-- delete account -->
+                <div class="account-delete">
+                    <p>Danger Zone</p>
+                    <form action="/settings/deleteAccount" method="post" onsubmit="return confirm('Czy na pewno chcesz usunąć konto? Operacja jest nieodwracalna.');">
+                        <button type="submit" class="btn btn--modal btn--delete">Delete Account</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+
     </main>
 
     <?php
