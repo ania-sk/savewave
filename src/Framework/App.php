09de8006ce7f1e72dff521369836421767d660b6
ania-sk <127092221+ania-sk@users.noreply.github.com>
2026-05-06 11:53:24 +0200

    Update App.php

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -22,8 +22,10 @@ class App
 
     public function run()
     {
-        $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        $method = $_SERVER['REQUEST_METHOD'];
+        $uri = $_SERVER['REQUEST_URI'] ?? '/';
+        $path = parse_url($uri, PHP_URL_PATH) ?? '/';
+
+        $method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
 
         $this->router->dispatch($path, $method, $this->container);
     }
